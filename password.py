68f2825a3b6ae2f2d821095f11118de14ed7ec93
SauceIsForever <164048334+SauceIsForever@users.noreply.github.com>
2025-03-20 14:10:42 -0400

    First Commit

---
+++ password.py
@@ -0,0 +1,19 @@
+import random
+
+# Character sets for password generation
+lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_case = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+special_chars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '/', '?']
+
+combined_list = lower_case + upper_case + numbers + special_chars
+
+new_password = ""
+
+password_length = int(input("Choose your password length: "))
+
+for char in range(password_length):
+    random_char = random.choice(combined_list)
+    new_password += random_char
+
+print(f"Here is your randomly generated password: {new_password}")
